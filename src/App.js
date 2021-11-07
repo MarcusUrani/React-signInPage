cb419be272ea05d76171685c631ff081fa3dcb40
Marcus Urani <marcus.urani20@gmail.com>
2021-11-07 13:13:13 -0300

    Finalizando o projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FormularioCadastro from "./components/FormularioCadastro";
+import { Container, Typography } from "@material-ui/core";
+import "@fontsource/roboto/400.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Container maxWidth="sm" component="article">
+        <Typography variant="h3" component="h1" align="center">
+          Formulário de cadastro
+        </Typography>
+        <FormularioCadastro aoEnviar={EnviarFormulario} validarCpf = {validaCPF}/>
+      </Container>
+    );
+  }
+}
+
+function EnviarFormulario(dados) {
+  console.log(dados);
+}
+
+function validaCPF(cpf) {
+  if(cpf.length !== 11) {
+    return {valido: false, texto: "O CPF deve ter 11 dígitos."}
+  } else {
+    return { valido: true, texto: "" };
+  }
 }
 
 export default App;
